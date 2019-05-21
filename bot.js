56bf2ea247974d301eb821c7371741cd4e4686fb
Cheong Wong <31480380+teddyw1024@users.noreply.github.com>
2019-05-21 13:06:35 -0400

    able to modify number of results

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ class MyBot extends ActivityHandler {
         this.name = null;
         this.index = null;
         this.specificValue = null;
-        
+        this.number = null;
         this.onMessage(async (context, next) => {
             const text = context.activity.text;
 
@@ -22,7 +22,8 @@ class MyBot extends ActivityHandler {
 
             if(this.name === null){
                 this.name = text;
-                await context.sendActivity(`Hi, ${this.name}.`);
+                console.log("1. set text as name");
+                await context.sendActivity(`Hi, ${this.name}. I am going to ask you serval question to help you find qualify jobs.`);
 
                 await context.sendActivity(`I am able to search by:
                                             \n 1. Posting type;
@@ -30,35 +31,60 @@ class MyBot extends ActivityHandler {
                                             \n 3. Full/Part Time Position;
                                             \n 4. Salary From
                                             \n 5. Salary To`);
-                this.sendSuggestedActions(context);
+                await context.sendActivity("how many results you want to get?")
+                console.log("2. ask for number of result");
                 await next();
-            }else if(this.index === null){
+            }else if(this.number === null){
+              console.log("3.number is empty");
+                if (typeof parseInt(text) != "number") {
+                  console.log("4. This is not number");
+                  await context.sendActivity("Sorry, please enter a number for results.")
+                }else{
+                  console.log("4. This is number type");
+                  //await context.sendActivity(`${this.name}, how many results you want to see?`);
+                  //await this.sendSuggestednumber(context);
+                  this.number = text;
+                  console.log("5. set text as number");
+                  console.log("number is equal");console.log(this.number);
+                  await context.sendActivity(`You have choose to get ${this.number} of results`);
+                  await context.sendActivity(`Next I am going to ask you some questions for me to seach for jobs, enter anything to let me know you are ready:`);
+                  //await context.sendActivity("Please choose the search option from above")//can't run the next message autometicly, has enter message to trigger
+                }
+            
+              }else if(this.index === null){
+              console.log("index is empty");
                 //await this.sendSuggestedActions(context);
                 if(validIndex.includes(text)){
+                    console.log("text is valid input for index");
                     this.index = text;
+                    console.log("set text as index");
                     await context.sendActivity(`${this.name}, you chose to search jobs by ${this.index}`);
                     if(this.index.includes("posting type")){
+                      console.log("index is chosen as posting type");
                         await context.sendActivity(`Please enter the specific value of Posting Type
                                                     \n1.Internal;
                                                     \n2.External;`);
-                        
                     }else if(this.index.includes("level")){
+                      console.log("index is chosen as level");
                         await context.sendActivity(`Please enter the specific value of Job Difficulty Level
                                                     \nNumber from 1~4`);
                     }else if(this.index.includes("Full/Part time")){
+                      console.log("index is chosen as full/part time");
                         await context.sendActivity(`Please enter the specific value of Full time/ Part time 
                                                     \n1.F
                                                     \n2.P`);
                     }else if(this.index.includes("Salary from")){
+                      console.log("index is chosen as salary from");
                         await context.sendActivity(`Please enter the specific value of Salary Starting point
                                                     \n`);
                     }else if(this.index.includes("salary to")){
-                        
+                      console.log("index is chosen as salary to");
                         await context.sendActivity(`Please enter the specific value of Salary Maximun
                                                     \n`);
                     }
                 
-                }else{
+                }else{console.log("text is not a valid input for index, first time always");
+                  console.log("ask user to enter again");
                     await context.sendActivity(`Please select a searching option:`);
                     await this.sendSuggestedActions(context);
                 }
@@ -66,70 +92,87 @@ class MyBot extends ActivityHandler {
                 await next();
                 
             }else if(this.specificValue === null){
+              console.log("specific value is empty");
                 //search by api
                 if(this.index == "posting type"){
                     console.log("check input valid posting type");
                     if(postingTypeArr.includes(text)){
-                        await context.sendActivity(`${this.name}, here are the top 3 result I found:`)
+                        await context.sendActivity(`${this.name}, here are the top ${this.number} results I found:`)
                         this.specificValue = text;
                         console.log("Posting type api")
                         await context.sendActivity(`You can search these jobs by Job ID on
                                                     \nhttp://www.nyc.gov/html/careers/html/search/search.shtml`);
-                        await this.getData1(text, context);
-                        await this.sendSuggestedActions(context);
-                        
+                        await this.getData1(text,this.number, context);
+                        this.index = null;
+                        this.specificValue = null;
+                        this.number = null;
+                        console.log("reset index and specificValue");
+                        await this.sendSuggestednumber(context);
+                        //await this.sendSuggestedActions(context);
                     }else{ await context.sendActivity(`${this.name}, I don't understand, please enter a valid input.`);}
                 }else if(this.index == "level"){
                     console.log("check input valid level");
                     if(levelArr.includes(text)){
-                        await context.sendActivity(`${this.name}, here are the top 3 result I found:`)
+                        await context.sendActivity(`${this.name}, here are the top ${this.number} results I found:`)
                         this.specificValue = text;
                         console.log("Level api")
                         await context.sendActivity(`You can search these jobs by Job ID on
                                                     \nhttp://www.nyc.gov/html/careers/html/search/search.shtml`);
-                        await this.getData2(text, context);
+                        await this.getData2(text,this.number, context);
                         this.index = null;
                         this.specificValue = null;
-                        await this.sendSuggestedActions(context);
+                        this.number = null;
+                        console.log("reset index and specificValue");
+                        await this.sendSuggestednumber(context);
+                        //await this.sendSuggestedActions(context);
                     }else{ await context.sendActivity(`${this.name}, I don't understand, please enter a valid input.`);}
                 }else if(this.index == "Full/Part time"){
                     console.log("check input valid fp indicator");
                     if(fpIndicatorArr.includes(text)){
-                        await context.sendActivity(`${this.name}, here are the top 3 result I found:`)
+                        await context.sendActivity(`${this.name}, here are the top ${this.number} results I found:`)
                         this.specificValue = text;
                         console.log("F/P api")
                         await context.sendActivity(`You can search these jobs by Job ID on
                                                     \nhttp://www.nyc.gov/html/careers/html/search/search.shtml`);
-                        await this.getData3(text, context);
+                        await this.getData3(text,this.number, context);
                         this.index = null;
                         this.specificValue = null;
-                        await this.sendSuggestedActions(context);
+                        this.number = null;
+                        console.log("reset index and specificValue");
+                        await this.sendSuggestednumber(context);
+                        //await this.sendSuggestedActions(context);
                     }else{ await context.sendActivity(`${this.name}, I don't understand, please enter a valid input.`);}
                 }else if(this.index == "Salary from"){
                     console.log("check input valid salary from");
                     if(!isNaN(text)){
-                        await context.sendActivity(`${this.name}, here are the top 3 result I found:`)
+                        await context.sendActivity(`${this.name}, here are the top ${this.number} results I found:`)
                         this.specificValue = text;
                         console.log("salary from api")
                         await context.sendActivity(`You can search these jobs by Job ID on
                                                     \nhttp://www.nyc.gov/html/careers/html/search/search.shtml`);
-                        await this.getData4(text, context);
+                        await this.getData4(text,this.number, context);
                         this.index = null;
                         this.specificValue = null;
-                        await this.sendSuggestedActions(context);
+                        this.number = null;
+                        console.log("reset index and specificValue");
+                        await this.sendSuggestednumber(context);
+                        //await this.sendSuggestedActions(context);
                     }else{ await context.sendActivity(`${this.name}, I don't understand, please enter a valid input.`);}
                 }else if(this.index == "Salary to"){
                     console.log("check input valid salary to");
                     if(!isNaN(text)){
-                        await context.sendActivity(`${this.name}, here are the top 3 result I found:`)
+                        await context.sendActivity(`${this.name}, here are the top ${this.number} results I found:`)
                         this.specificValue = text;
                         console.log("salary to api")
                         await context.sendActivity(`You can search these jobs by Job ID on
                                                     \nhttp://www.nyc.gov/html/careers/html/search/search.shtml`);
-                        await this.getData5(text, context);
+                        await this.getData5(text,this.number, context);
                         this.index = null;
                         this.specificValue = null;
-                        await this.sendSuggestedActions(context);
+                        this.number = null;
+                        console.log("reset index and specificValue");
+                        await this.sendSuggestednumber(context);
+                        //await this.sendSuggestedActions(context);
                         
                     }else{ await context.sendActivity(`${this.name}, I don't understand, please enter a valid input.`);}
                 }
@@ -164,15 +207,23 @@ class MyBot extends ActivityHandler {
     async sendSuggestedActions(turnContext) {
         var reply = MessageFactory.suggestedActions(
           ["posting type", "level", "Full/Part time", "Salary from", "Salary to"],
-          "What do you want to search?"
+          "What do you want to search this time?"
         );
         await turnContext.sendActivity(reply);
     }
 
+    async sendSuggestednumber(turnContext) {
+      var reply = MessageFactory.suggestedActions(
+        ["1", "2", "3", "4", "5"],
+        "How many results you want to search?"
+      );
+      await turnContext.sendActivity(reply);
+  }
 
-    async getData1(type, context) {//Posting Type
+
+    async getData1(type,number, context) {//Posting Type
         const message = await new Promise((resolve, reject) => {
-          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=3&posting_type=${type}`;
+          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=${number}&posting_type=${type}`;
           console.log(context);
           axios
             .get(url)
@@ -219,10 +270,11 @@ class MyBot extends ActivityHandler {
         }
       }
 
-      async getData2(type, context) {//Job Difficulty Level
+      async getData2(type,number, context) {//Job Difficulty Level
         const message = await new Promise((resolve, reject) => {
-          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=3&level=${type}`;
+          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=${number}&level=${type}`;
           console.log(context);
+          console.log("number is put in data2");
           axios
             .get(url)
             .then(response => {
@@ -268,9 +320,9 @@ class MyBot extends ActivityHandler {
         }
       }
 
-    async getData3(type, context) {//Full time or Part time
+    async getData3(type,number, context) {//Full time or Part time
         const message = await new Promise((resolve, reject) => {
-          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=3&full_time_part_time_indicator=${type}`;
+          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=${number}&full_time_part_time_indicator=${type}`;
           console.log(context);
           axios
             .get(url)
@@ -317,9 +369,9 @@ class MyBot extends ActivityHandler {
         }
       }
       
-      async getData4(type, context) {//Salary From
+      async getData4(type,number, context) {//Salary From
         const message = await new Promise((resolve, reject) => {
-          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=3&salary_range_from=${type}`;
+          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=${number}&salary_range_from=${type}`;
           console.log(context);
           axios
             .get(url)
@@ -366,9 +418,9 @@ class MyBot extends ActivityHandler {
         }
       }
 
-      async getData5(type, context) {//Salary To
+      async getData5(type,number, context) {//Salary To
         const message = await new Promise((resolve, reject) => {
-          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=3&salary_range_to=${type}`;
+          let url = `https://data.cityofnewyork.us/resource/kpav-sd4t.json?$limit=${number}&salary_range_to=${type}`;
           console.log(context);
           axios
             .get(url)
